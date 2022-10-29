518a91398aa88c66b63c7a30c194f8077d817558
Rodolfo F.Moreira <rodolfofm@msn.com>
2022-10-29 07:25:39 -0400

    up first

---
+++ start-etl-dw.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/opt/pdi-ce-8.3.0.0-371/data-integration/kitchen.sh /file:/home/innovation_user/etl/job_principal_innovation_dw.kjb \
+/level:Minimal 
---
+++ start-etl-unico.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/opt/pdi-ce-8.3.0.0-371/data-integration/pan.sh /file:$1 \
+/level:Basic 
