8c4b6e0ae1c96ce265053c5dc6724da9be55a026
Rodolfo F.Moreira <rodolfofm@msn.com>
2022-10-29 07:58:13 -0400

    incluido c.medio p/ calc marg.liquida, add filtro pedidos faturados

--- start-etl-dw.sh
+++ start-etl-dw.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+git pull "https://rodolfofm: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@github.com/rodolfofm/innovation-etl.git" main
 /opt/pdi-ce-8.3.0.0-371/data-integration/kitchen.sh /file:/home/innovation_user/etl/job_principal_innovation_dw.kjb \
 /level:Minimal 
